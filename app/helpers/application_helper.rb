7bb4824e9cfebd2f2210f2d6691beab6c38a3029
Silvio Cavalcanti Bonfim <silviocavalcanti2011@gmail.com>
2024-09-15 17:41:19 -0300

    Adicionado e configurado o overcommit para trabalhar com o rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# O ApplicationHelper fornece métodos auxiliares para serem utilizados nas views
 module ApplicationHelper
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
+# O ApplicationMailer é a classe base para todos os mailers na aplicação.
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  default from: 'from@example.com'
+  layout 'mailer'
 end
