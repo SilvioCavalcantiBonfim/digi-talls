ebabb4459dc838903108426c8ac1005d7abda1f4
silvio cavalcanti <silviocavalcanti2011@gmail.com>
2024-09-15 21:15:12 -0300

    Adicionado as validacoes do model faq

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,2 +1,5 @@
 class Faq < ApplicationRecord
+  validate :question, presence: true, length: { minimum: 3 }
+  validate :answer, presence: true, length: { minimum: 3 }
+  validate :is_active, presence: true
 end
