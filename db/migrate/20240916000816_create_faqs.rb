793cd54816e0b0263e2e4791c4325b0771afe8b6
silvio cavalcanti <silviocavalcanti2011@gmail.com>
2024-09-15 21:10:32 -0300

    Adicionado as validacoes na migrate

--- db/migrate/20240916000816_create_faqs.rb
+++ db/migrate/20240916000816_create_faqs.rb
@@ -1,9 +1,9 @@
 class CreateFaqs < ActiveRecord::Migration[7.0]
   def change
     create_table :faqs do |t|
-      t.string :question
-      t.text :answer
-      t.boolean :is_active
+      t.string :question, null: false
+      t.text :answer, null: false
+      t.boolean :is_active, null: false, default: false
 
       t.timestamps
     end
