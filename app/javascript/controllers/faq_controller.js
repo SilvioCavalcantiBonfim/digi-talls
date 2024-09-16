377a5207149b84f60f9b46b722d7ec79ee16408e
Silvio Cavalcanti Bonfim <silviocavalcanti2011@gmail.com>
2024-09-16 11:57:05 -0300

    criado o componente da faq

--- app/javascript/controllers/faq_controller.js
+++ app/javascript/controllers/faq_controller.js
@@ -2,15 +2,28 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="faq"
 export default class extends Controller {
-
-  static targets = ["content", "icon"]
+  static targets = ["content", "icon", "button"];
 
   toggle() {
-    this.contentTarget.classList.toggle("hidden")
-    if (this.contentTarget.classList.contains("hidden")) {
-      this.iconTarget.textContent = "expand_more"
+    const content = this.contentTarget;
+    const icon = this.iconTarget;
+    const button = this.buttonTarget;
+
+    button.classList.toggle("faq-color");
+    icon.classList.toggle("rotate-180");
+
+    const isHidden = content.classList.contains("hidden");
+
+    if (isHidden) {
+      content.classList.remove("hidden");
+      content.classList.remove("scale-out-ver-top");
+      content.classList.add("scale-in-ver-top");
     } else {
-      this.iconTarget.textContent = "expand_less"
+      content.classList.remove("scale-in-ver-top");
+      content.classList.add("scale-out-ver-top");
+      setTimeout(() => {
+        content.classList.add("hidden");
+      }, 200);
     }
   }
 }
