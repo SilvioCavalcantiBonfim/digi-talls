061b19951f2c4d10a108cf1ac2e0c8bfbabd7691
Silvio Cavalcanti Bonfim <silviocavalcanti2011@gmail.com>
2024-09-17 10:55:22 -0300

    Criado a rota e melhorado o botao de exclusao de faqs

--- app/controllers/admin/faqs_controller.rb
+++ app/controllers/admin/faqs_controller.rb
@@ -4,6 +4,16 @@ class Admin::FaqsController < ApplicationController
     @faqs = Faq.all
   end
 
+  # DELETE admin/faqs/1 or admin/faqs/1.json
+  def destroy
+    @faq.destroy
+
+    respond_to do |format|
+      format.html { redirect_to admin_faqs_path, status: :see_other, notice: 'Faq was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
 
   # Use callbacks to share common setup or constraints between actions.
