09bb517d29fcb9989f4d5e5f2e01f1af133208b4
Silvio Cavalcanti Bonfim <silviocavalcanti2011@gmail.com>
2024-09-25 14:38:58 -0300

    Criado a barra de navegacao de administracao

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@
 
 # O ApplicationHelper fornece métodos auxiliares para serem utilizados nas views
 module ApplicationHelper
+  def first_letter(string)
+    string[0].upcase if string.present?
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module DigiTalls
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.i18n.default_locale = :pt
+    config.i18n.default_locale = :en
 
     # Configuration for the application, engines, and railties goes here.
     #
