b81fbba4c00a03c8a850bc06afd3849252a99166
Silvio Cavalcanti Bonfim <silviocavalcanti2011@gmail.com>
2024-10-06 19:50:43 -0400

    Criado o menu de navegacao global

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module DigiTalls
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :pt
 
     # Configuration for the application, engines, and railties goes here.
     #
