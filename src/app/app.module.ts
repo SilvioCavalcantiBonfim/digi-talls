9051a0414c4e48900f9d05082d8186ca1c68ba43
Silvio Cavalcanti Bonfim <silviocavalcanti2011@gmail.com>
2024-12-07 11:04:48 -0300

    create header and update font base to inter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterModule } from './components/footer/footer.module';
+import { HeaderModule } from "./components/header/header.module";
 
 @NgModule({
   declarations: [
@@ -12,8 +13,9 @@ import { FooterModule } from './components/footer/footer.module';
   imports: [
     BrowserModule,
     FooterModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    HeaderModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/logo/logo.module.ts
+++ src/app/components/logo/logo.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterLogoComponent } from './footer-logo/footer-logo.component';
+import { HeaderLogoComponent } from './header-logo/header-logo.component';
 
 
 
 @NgModule({
   declarations: [
-    FooterLogoComponent
+    FooterLogoComponent,
+    HeaderLogoComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [FooterLogoComponent]
+  exports: [FooterLogoComponent, HeaderLogoComponent]
 })
 export class LogoModule { }
